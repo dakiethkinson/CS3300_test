a27b445fe1f8f5baa1cc3caf8c7a5f72fa8da37b
root <dakiethkinson@gmail.com>
2022-11-29 01:16:07 +0000

    Devise Login tests complete

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,85 +1,111 @@
 
 RSpec.feature "Visiting Projects Page", type: :feature do
-  scenario "The visitor should see projects" do
-    visit new_project_path
-    expect(page).to have_text("New project")
+  scenario "The visitor should sign in" do
+    user = create(:user)
   end
 
-  scenario "The visitor should see title block" do
-    visit new_project_path
-    expect(page).to have_text("Title")
-  end
+  context "Sign in user" do
+    before(:each) do
+      visit new_project_path
+      within(id = "new_user") do
+        fill_in "email", with: user.email
+        fill_in "password", with: user.password
+        click_button "Log In"
+        expect(page).to have_text "Signed in successfully."
+      end
 
-  scenario "The visitor should see description block" do
-    visit new_project_path
-    expect(page).to have_text("Description")
-  end
+      scenario "The visitor should see projects" do
+        visit new_project_path
+        expect(page).to have_text("New project")
+      end
 
-  scenario "The visitor should see Back to projects link" do
-    visit new_project_path
-    expect(page).to have_link("Back to projects")
-  end
+      scenario "The visitor should see title block" do
+        visit new_project_path
+        expect(page).to have_text("Title")
+      end
 
-  scenario "The visitor should see Create Project Button" do
-    visit new_project_path
-    expect(page).to have_button("Create Project")
-  end
+      scenario "The visitor should see description block" do
+        visit new_project_path
+        expect(page).to have_text("Description")
+      end
 
+      scenario "The visitor should see Back to projects link" do
+        visit new_project_path
+        expect(page).to have_link("Back to projects")
+      end
+
+      scenario "The visitor should see Create Project Button" do
+        visit new_project_path
+        expect(page).to have_button("Create Project")
+      end
+    end
+  end
 end
 
 RSpec.feature "Projects", type: :feature do
-  
-  context "Create new project" do
+  context "Sign in user" do
     before(:each) do
       visit new_project_path
-      within("form") do
-        fill_in "Title", with: "Test title"
+      within(id = "new_user") do
+        fill_in "email", with: user.email
+        fill_in "password", with: user.password
+        click_button "Log In"
+        expect(page).to have_text "Signed in successfully."
       end
-    end
 
-    scenario "should be successful" do
-      fill_in "Description", with: "Test description"
-      click_button "Create Project"
-      expect(page).to have_content("Project was successfully created")
-    end
-
-    scenario "should fail" do
-      click_button "Create Project"
-      expect(page).to have_content("Description can't be blank")
-    end
-  end
-
-  context "Update project" do
-    let(:project) { Project.create(title: "Test title", description: "Test content") }
-    before(:each) do
-      visit edit_project_path(project)
-    end
-
-    scenario "should be successful" do
-      within("form") do
-        fill_in "Description", with: "New description content"
+      context "Create new project" do
+        before(:each) do
+          visit new_project_path
+          within("form") do
+            fill_in "Title", with: "Test title"
+          end
+        end
+    
+        scenario "should be successful" do
+          fill_in "Description", with: "Test description"
+          click_button "Create Project"
+          expect(page).to have_content("Project was successfully created")
+        end
+    
+        scenario "should fail" do
+          click_button "Create Project"
+          expect(page).to have_content("Description can't be blank")
+        end
       end
-      click_button "Update Project"
-      expect(page).to have_content("Project was successfully updated")
-    end
 
-    scenario "should fail" do
-      within("form") do
-        fill_in "Description", with: ""
+      context "Update project" do
+        let(:project) { Project.create(title: "Test title", description: "Test content") }
+        before(:each) do
+          visit edit_project_path(project)
+        end
+    
+        scenario "should be successful" do
+          within("form") do
+            fill_in "Description", with: "New description content"
+          end
+          click_button "Update Project"
+          expect(page).to have_content("Project was successfully updated")
+        end
+    
+        scenario "should fail" do
+          within("form") do
+            fill_in "Description", with: ""
+          end
+          click_button "Update Project"
+          expect(page).to have_content("Description can't be blank")
+        end
       end
-      click_button "Update Project"
-      expect(page).to have_content("Description can't be blank")
-    end
-  end
 
-  context "Remove existing project" do
-      let!(:project) { Project.create(title: "Test title", description: "Test content") }
-      scenario "remove project" do
-      visit project_path(project)
-      click_button "Destroy this project"
-      expect(page).to have_content("Project was successfully destroyed")
-      expect(Project.count).to eq(0)
-      
+      context "Remove existing project" do
+        let!(:project) { Project.create(title: "Test title", description: "Test content") }
+        scenario "remove project" do
+        visit project_path(project)
+        click_button "Destroy this project"
+        expect(page).to have_content("Project was successfully destroyed")
+        expect(Project.count).to eq(0)
+        
+      end
+    end      
     end
-  end
+  end  
 end
